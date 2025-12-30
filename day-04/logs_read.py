8607ab41fe83d88d69436c2fa48a68bf911954ae
Duryab333 <duryabtariq333@gmail.com>
2025-12-30 01:39:36 +0100

    day04 task is done

---
+++ day-04/logs_read.py
@@ -0,0 +1,38 @@
+import pdb
+import json
+def read_logs():
+    with open("./app.log","r") as file:
+        return file.readlines()
+    
+
+def analyzer(lines):
+    #pdb.set_trace()
+    log_count ={
+        "INFO": 0,
+        "WARNING": 0,
+        "ERROR" :0
+    }
+    for line in lines:
+        if "INFO" in line:
+            log_count.update({"INFO" : log_count["INFO"] + 1})
+        elif "WARNING" in line:
+            log_count.update({"WARNING" :  log_count["WARNING"] + 1 })
+        elif "ERROR" in line:
+            log_count.update({"ERROR" : log_count["ERROR"]+1})
+        else:
+            pass
+    return log_count
+
+def write_json(counts):
+    with open("output.json", "w+") as json_file:
+        json.dump(counts, json_file)
+
+lines = read_logs()
+counts = analyzer(lines)
+write_json(counts)
+
+
+print(f"log count are : {counts}")
+
+
+
